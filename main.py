a5fd392ee7ce5500007414b1acf18191fc422380
Faithfayth <169154594+Faithfayth@users.noreply.github.com>
2026-09-06 02:09:15 -0800

    learning python

---
+++ main.py
@@ -0,0 +1,13 @@
+print ("Hello world!")
+
+# name = "Peter"
+
+# print ("My name is " + name)
+
+# name2 = input("Hey there, enter your name here: ")
+# print ("Your name is " + name2)
+
+age = input("How old are you? ")
+name2 = input("Hey there, enter your name here: ")
+
+print ("So " + name2 + ", you are " + age + " years old.")
