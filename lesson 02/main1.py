177025d9af54de76c1c2d2733c8da07b3ed3e8af
Faithfayth <169154594+Faithfayth@users.noreply.github.com>
2026-09-07 07:52:51 -0800

    variables, in-built eg len() & sum(), list, dict

--- lesson 02/main1.py
+++ lesson 02/main1.py
@@ -1,1 +1,35 @@
 #--------------STUDENT GRADE MANAGER--------------
+name = input("Enter your name: ")
+age = int(input("Enter yoyr age: "))
+
+print("\n ---------Student information ----------")
+print("Name: ", name)
+print("Age: ", age)
+
+subjects = ["Math", "Python", "English", "Science"]
+
+print("\n Subjects: ")
+for subject in subjects:
+    print(subject)
+else: print("\n")
+    
+marks = {
+    "Math": 50,
+    "Python": 70,
+    "English": 40,
+    "Science": 58,
+}
+
+for subject, mark in marks.items():
+    print( subject, ":", mark)
+
+total = sum(marks.values())
+average = total /  len(marks)
+print("\nAverage: ", average)
+
+if average >= 50:
+    passed = True
+    print("Status: PASSED")
+else:
+    passed = False
+    print("Status: FAILED")
